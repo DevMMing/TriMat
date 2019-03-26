b424cf005ee5064d70f0789a783086ce5753b13d
DevMMing <mming@stuy.edu>
2019-03-26 01:05:47 -0400

    base * 2

--- gmath.py
+++ gmath.py
@@ -4,13 +4,16 @@ from display import *
 
 
 #vector functions
-#normalize vetor, should modify the parameter
+#normalize vector, should modify the parameter
 def normalize(vector):
-    pass
-
+    length = math.sqrt(math.pow(vector[0],2)+math.pow(vector[1],2)+math.pow(vector[2],2))
+    vector[0]/=length
+    vector[1]/=length
+    vector[2]/=length
+    return vector
 #Return the dot porduct of a . b
 def dot_product(a, b):
-    return 0
+    return a[0]*b[0]+a[1]*b[1]+a[2]*b[2]
 
 #Calculate the surface normal for the triangle whose first
 #point is located at index i in polygons
