f532c94c7d6a27d277d20eaa5f2ab7ee81017880
DevMMing <mming@stuy.edu>
2019-03-27 11:11:12 -0400

    almost done

--- draw.py
+++ draw.py
@@ -65,13 +65,15 @@ def add_sphere(polygons, cx, cy, cz, r, step ):
     for lat in range(lat_start, lat_stop):
         for longt in range(longt_start, longt_stop+1):
             index = lat * step + longt
-
-            add_edge(polygons, points[index][0],
+            add_polygon(polygons, points[index][0],
                      points[index][1],
                      points[index][2],
-                     points[index][0]+1,
-                     points[index][1]+1,
-                     points[index][2]+1 )
+                     points[index-1][0],
+                     points[index-1][1],
+                     points[index-1][2],
+                     points[index-step-1][0],
+                     points[index-step-1][1],
+                     points[index-step-1][2] )
 
 def generate_sphere( cx, cy, cz, r, step ):
     points = []
@@ -106,12 +108,15 @@ def add_torus(polygons, cx, cy, cz, r0, r1, step ):
         for longt in range(longt_start, longt_stop):
             index = lat * step + longt
 
-            add_edge(polygons, points[index][0],
+            add_polygon(polygons, points[index][0],
                      points[index][1],
                      points[index][2],
-                     points[index][0]+1,
-                     points[index][1]+1,
-                     points[index][2]+1 )
+                     points[index-11][0],
+                     points[index-1][1],
+                     points[index-1][2],
+                     points[index-step][0],
+                     points[index-step][1],
+                     points[index-step][2] )
 
 def generate_torus( cx, cy, cz, r0, r1, step ):
     points = []
